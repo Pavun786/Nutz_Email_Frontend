1e4259029a4e91d2b5a97f69fc826bafa467d43a
Pavun786 <pulsarpavuns786@gmail.com>
2023-06-26 01:09:04 -0700

    tablemodify

--- src/EmailList.js
+++ src/EmailList.js
@@ -5,118 +5,86 @@ import Button from '@mui/material/Button';
 import { Link } from "react-router-dom";
 
 
-function EmailList({data,setData}){
+function EmailList({data,setData}) {
 
     const[email,setEmail]=useState([])
-    const [isLoading, setLoading] = useState(false);
-
-   useEffect(()=>{
-     loadData()
-   },[])
-
-   let loadData=async()=>{
+        const [isLoading, setLoading] = useState(false);
+    
+       useEffect(()=>{
+         loadData()
+       },[])
+    
+       let loadData=async()=>{
+          
+          setLoading(true);
+    
+          await fetch("https://nutz-email-backend-bice.vercel.app/emails",{
+            method:"GET"
+          })
+          .then((data)=>data.json())
+          .then((info)=> setEmail(info))
+    
+           setLoading(false);
+           
+      }
       
-      setLoading(true);
-
-      await fetch("https://nutz-email-backend-bice.vercel.app/emails",{
-        method:"GET"
-      })
-      .then((data)=>data.json())
-      .then((info)=> setEmail(info))
-
-       setLoading(false);
-       
-  }
-  
-
-  const inboxView =(email)=>{
-    setData(email);
-    console.log(email)
-  }
-
-    return(
-
-
-
-        <div class="container-fluid">
-        
+    
+      const inboxView =(email)=>{
+        setData(email);
+        console.log(email)
+      }
+  return (
+    <>
          <a className="refresh"
           href="/list"
           >
             <Button  variant="contained">Refresh{" "}</Button>
          </a>
         
-     {isLoading ? (
+         {isLoading ? (
         
-        <span className="loading">Loading...<img className="loadimage" src="https://usagif.com/wp-content/uploads/loading-29.gif"/></span>
-
-      ) : ( <div class="card-body">
-      <div class="table-responsive">
-          <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                <span className="loading">Loading...<img className="loadimage" src="https://usagif.com/wp-content/uploads/loading-29.gif"/></span>
+        
+              ) :( <table class="table">
               <thead>
                   <tr>
-                      
                       <th>ID</th>
-                      <th>sender</th>
-                      <th>receiver</th>
-                      <th>subject</th>
-                      <th>Date</th>
-                      
-                      
+                       <th>sender</th>
+                       <th>receiver</th>
+                       <th>subject</th>
+                       <th>Date</th>
                   </tr>
               </thead>
-              <tfoot>
-                  <tr>
-                      
-                     <th>ID</th>
-                      <th>sender</th>
-                      <th>receiver</th>
-                      <th>subject</th>
-                      <th>Date</th>
-                      
-                  </tr>
-              </tfoot>
-             <tbody>
-             
-              
-            
+              <tbody>
+                 
               {email.map((email,index)=>{
-                  return <tr key={index}>
+                   return <tr key={index}>
 
-                      <td>{email.id}</td>
-                      <td>{email.from}</td>
-                      <td>{email.to}</td>
-                      <td>{email.subject}</td>
-                      <td>{email.date}</td>
+                       <td>{email.id}</td>
+                       <td>{email.from}</td>
+                       <td>{email.to}</td>
+                       <td>{email.subject}</td>
+                       <td>{email.date}</td>
                       
 
-                      <td>
-                              <Link to="/view">
+                       <td>
+                               <Link to="/view">
                                
-                              <Button onClick={()=>inboxView(email)} variant="contained">View</Button>
+                               <Button onClick={()=>inboxView(email)} variant="contained">View</Button>
                               
-                              </Link> 
+                               </Link> 
                                
-                           </td>
-                  </tr>
-              })}
-              </tbody> 
-                  
-              
-          </table>
-      </div>
-  </div>)}
-
-      
-   </div>
-        
-        
-      
-
-
-    )
+                            </td>
+                   </tr>
+              })}  
+              </tbody>
+            </table>
+         
+         )}
+    
+    </>
+  )
 }
+
 export default EmailList;
-
-
 
